fff436c6f4dedb8e4c6ef4436285874014a6fb72
ohamin26 <ohamin26@gmail.com>
2023-07-25 15:55:07 +0900

    수정

--- app/componets/element/skills.tsx
+++ app/componets/element/skills.tsx
@@ -8,56 +8,53 @@ import img4 from '@/public/images_black/next.jpg'
 import img5 from '@/public/images_black/react.jpg'
 import img6 from '@/public/images_black/recoil.jpg'
 import img7 from '@/public/images_black/ts.jpg'
-
-import color_img1 from '@/public/images/css.jpg'
-import color_img2 from '@/public/images/html.jpg'
-import color_img3 from '@/public/images/js.jpg'
-import color_img4 from '@/public/images/next.jpg'
-import color_img5 from '@/public/images/react.jpg'
-import color_img6 from '@/public/images/recoil.jpg'
-import color_img7 from '@/public/images/ts.jpg'
+import color_img1 from '@/public/images/css.png'
+import color_img2 from '@/public/images/html.png'
+import color_img3 from '@/public/images/js.png'
+import color_img4 from '@/public/images/next.png'
+import color_img5 from '@/public/images/react.png'
+import color_img6 from '@/public/images/recoil.png'
+import color_img7 from '@/public/images/ts.png'
 export default function skills() {
     return (
         <div className="flex justify-center xs:w-[70vw]" {...useScroll('right', 1, 0)}>
-            <div className="card_skills">
+            <div className="card_skills place-items-center">
                 <div className="grid justify-center">
-                    <div className="grid items-center card_skill text-center">
+                    <div className="grid items-center card_skill text-center lg:mt-28">
                         <p className="text-6xl xs:text-3xl">skills</p>
                     </div>
                 </div>
-                <hr className="mt-0 pt-0" />
                 <div
-                    className="w-full h-fit grid lg:grid-rows-2 lg:grid-cols-4 place-items-center
+                    className="w-4/5 h-fit grid lg:grid-rows-2 lg:grid-cols-4 place-items-center
                 md:grid-rows-3 md:grid-cols-3 
                 sm:grid-rows-4 sm:grid-cols-2 
-                xs:grid-rows-6 xs:grid-cols-1 
-                items-center justify-center"
+                xs:grid-rows-6 xs:grid-cols-1"
                 >
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img1} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img1} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img2} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img2} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img3} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img3} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img4} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img4} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img5} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img5} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img6} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img6} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
-                    <div className="relative">
+                    <div className="relative lg:left-10 md:left-10">
                         <Image src={img7} width={0} height={0} alt="Pic" className="card_img"></Image>
                         <Image src={color_img7} width={0} height={0} alt="Pic" className="card_color_img"></Image>
                     </div>
